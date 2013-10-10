823ec727d876396c814a461f412094e98361f919
Kevin Rockwood <kevin@rockwood.me>
2013-10-10 16:15:24 -0400

    Refactor FFMpeg so runner doesn't need to be passed in

--- lib/ffsplitter/ffmpeg.rb
+++ lib/ffsplitter/ffmpeg.rb
@@ -1,24 +1,26 @@
 module FFSplitter
   class FFMpeg
-    def initialize(input_file, runner=CommandRunner)
-      @input_file = input_file
-      @runner = runner
+    attr_accessor :filename, :runner
+
+    def initialize(filename)
+      self.filename = filename
+      runner = CommandRunner
     end
 
     def encode(chapters)
       chapters.each do |chapter|
-        @runner.run(chapter_command(chapter))
+        runner.run(chapter_command(chapter))
       end
     end
 
     def read_metadata
-      @runner.run("ffmpeg -i #{@input_file} -v quiet -f ffmetadata -")
+      runner.run("ffmpeg -i #{filename} -v quiet -f ffmetadata -")
     end
 
     private
 
     def chapter_command(chapter)
-      "ffmpeg -i #{@input_file} -ss #{chapter.start_time} -to #{chapter.end_time} -c copy '#{chapter.filename}.mp4'"
+      "ffmpeg -i #{filename} -ss #{chapter.start_time} -to #{chapter.end_time} -c copy '#{chapter.filename}.mp4'"
     end
   end
 end
--- spec/ffmpeg_spec.rb
+++ spec/ffmpeg_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 module FFSplitter
   describe FFMpeg do
     let(:runner) { double("runner", :accept => true) }
-    let(:ffmpeg){ FFMpeg.new("test.mp4", runner) }
+    let(:ffmpeg){ FFMpeg.new("test.mp4") }
+    before { ffmpeg.runner = runner }
 
     describe "#encode" do
       let(:chapter){ Chapter.new(start_frames: 10, end_frames: 20, timebase: 1, title: "test title") }
