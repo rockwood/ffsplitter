6f219f167ebc7147ffbe5852325c6fa42da3d84f
Kevin Rockwood <kevin@rockwood.me>
2013-10-10 22:46:53 -0400

    Refactor ffmpeg spec

--- spec/ffmpeg_spec.rb
+++ spec/ffmpeg_spec.rb
@@ -2,22 +2,23 @@ require 'spec_helper'
 
 module FFSplitter
   describe FFMpeg do
-    let(:runner) { double("runner", :accept => true) }
     let(:ffmpeg){ FFMpeg.new("test.mp4") }
+    let(:runner) { double("runner") }
     before { ffmpeg.runner = runner }
 
     describe "#encode" do
-      let(:chapter){ Chapter.new(start_frames: 10, end_frames: 20, timebase: 1, title: "test title") }
+      let(:chapter_list) { ChapterList.new }
+      let(:chapter) { Chapter.new(start_frames: 10, end_frames: 20, timebase: 1, title: "title") }
+      before { chapter_list.add(chapter) }
       it "encodes the chapters" do
-        expect(runner).to receive(:run).with("ffmpeg -i test.mp4 -ss 10.0 -to 20.0 -c copy '#{chapter.filename}.mp4'")
-        ffmpeg.encode([chapter])
+        expect(runner).to receive(:run).with "ffmpeg -i test.mp4 -ss 10.0 -to 20.0 -c copy '01 title.mp4'"
+        ffmpeg.encode(chapter_list)
       end
     end
 
     describe "#read_metadata" do
-      let(:filename) { "test.mp4" }
       it "reads the metadata" do
-        expect(runner).to receive(:run).with("ffmpeg -i #{filename} -v quiet -f ffmetadata -")
+        expect(runner).to receive(:run).with("ffmpeg -i test.mp4 -v quiet -f ffmetadata -")
         ffmpeg.read_metadata
       end
     end
