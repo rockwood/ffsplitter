90552353d14e2d16c7d089224600a35a4f27d404
Kevin Rockwood <kevin@rockwood.me>
2013-10-13 17:18:57 -0400

    Move spec/tmp to tmp

--- spec/end_to_end_spec.rb
+++ spec/end_to_end_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 module FFSplitter
   describe "End to end" do
-    before { Dir.mkdir("spec/tmp") unless Dir.exist?("spec/tmp") }
+    before { Dir.mkdir("tmp") unless Dir.exist?("tmp") }
 
     describe "split" do
-      before { `bundle exec ruby -Ilib bin/ffsplitter 'spec/fixtures/test video.mp4' spec/tmp/` }
-      let(:file_list) { Dir.glob("spec/tmp/*")  }
+      before { `bundle exec ruby -Ilib bin/ffsplitter 'spec/fixtures/test video.mp4' tmp` }
+      let(:file_list) { Dir.glob("tmp/*")  }
       it "creates 4 files" do
-        expect(file_list).to include("spec/tmp/01 Chapter 1.mp4")
-        expect(file_list).to include("spec/tmp/02 - Chapter 2.mp4")
-        expect(file_list).to include("spec/tmp/03 Chapter 3.mp4")
+        expect(file_list).to include("tmp/01 Chapter 1.mp4")
+        expect(file_list).to include("tmp/02 - Chapter 2.mp4")
+        expect(file_list).to include("tmp/03 Chapter 3.mp4")
       end
     end
 
