b0a928302dc3df07e846752ddc8127bb2ea3f36f
Kevin Rockwood <kevin@rockwood.me>
2013-10-13 18:26:59 -0400

    Test ffmpeg.chapter_command and refactor related tests

--- spec/ffmpeg_spec.rb
+++ spec/ffmpeg_spec.rb
@@ -10,27 +10,50 @@ module FFSplitter
 
     describe "#encode" do
       let(:chapter_list) { ChapterList.new }
-      let(:chapter) { Chapter.new(start_frames: 10, end_frames: 20, timebase: 1, title: "title") }
-      before { chapter_list.add(chapter) }
-      context "without an output directory" do
-        it "encodes the chapters" do
-          expect(runner).to receive(:run).with(/-i 'test.mp4' -ss 10.0 -to 20.0/)
-          ffmpeg.encode(chapter_list)
-        end
-      end
-      context "with an output directory" do
-        let(:output_directory) { "spec/tmp/" }
-        it "encodes the chapters with an output directory" do
-          expect(runner).to receive(:run).with(/spec\/tmp\/01 title.mp4/)
+      let(:chapter1) { Chapter.new(title: "Chapter 1") }
+      let(:chapter2) { Chapter.new(title: "Chapter 2") }
+
+      context "with a single chapter" do
+        before { chapter_list.add(chapter1) }
+        it "encodes the chapter" do
+          expect(runner).to receive(:run).with("ffmpeg #{ffmpeg.chapter_command(chapter1)}")
           ffmpeg.encode(chapter_list)
         end
       end
 
-      context "with multiple files" do
-        before { chapter_list.add(Chapter.new(title: "Chapter 2")) }
+      context "with multiple chapters" do
+        before do
+          chapter_list.add(chapter1)
+          chapter_list.add(chapter2)
+        end
         it "encodes the chapters with a single ffmpeg command" do
-          expect(runner).to receive(:run).once()
+          commands = "#{ffmpeg.chapter_command(chapter1)} #{ffmpeg.chapter_command(chapter2)}"
+          expect(runner).to receive(:run).once.with("ffmpeg #{commands}")
           ffmpeg.encode(chapter_list)
+        end
+      end
+    end
+
+    describe "#chapter_command" do
+      let(:command) { ffmpeg.chapter_command(chapter) }
+      let(:chapter) { Chapter.new(start_frames: 10, end_frames: 20, timebase: 1, title: "test title") }
+
+      context "without an output directory" do
+        let(:output_directory) { nil }
+        it "creates a chapter command" do
+          expect(command).to match("-i 'test.mp4'")
+          expect(command).to match("-ss 10.0")
+          expect(command).to match("-to 20.0")
+          expect(command).to match("-c copy")
+          expect(command).to match("-v error")
+          expect(command).to match("#{File.expand_path(chapter.filename)}")
+        end
+      end
+
+      context "with an output directory" do
+        let(:output_directory) { "test_dir" }
+        it "creates returns the path to that directory" do
+          expect(command).to match("#{File.expand_path(chapter.filename, "test_dir")}")
         end
       end
     end
