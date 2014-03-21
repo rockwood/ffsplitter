7d2220577adec6e59597969c958e736e576b9c0c
Kevin Rockwood <kevin@rockwood.me>
2014-03-20 20:35:21 -0400

    Have ffmpeg take an output_extension

--- lib/ffsplitter/ffmpeg.rb
+++ lib/ffsplitter/ffmpeg.rb
@@ -1,11 +1,12 @@
 module FFSplitter
   class FFMpeg
     CODEC_OPTIONS= "-c copy -movflags faststart"
-    attr_accessor :filename, :output_path, :runner
+    attr_accessor :filename, :output_path, :runner, :output_extension
 
     def initialize(options)
       @filename = options[:filename]
       @output_path = options[:output_path]
+      @output_extension = options[:output_extension]
       @runner = CommandRunner
     end
 
@@ -20,7 +21,7 @@ module FFSplitter
 
     def chapter_command(chapter)
       output_file = File.expand_path(chapter.filename, output_path)
-      "ffmpeg -ss #{chapter.start_time} -i '#{filename}' -t #{chapter.duration} #{CODEC_OPTIONS} '#{output_file}.mp4'"
+      "ffmpeg -ss #{chapter.start_time} -i '#{filename}' -t #{chapter.duration} #{CODEC_OPTIONS} '#{output_file}#{output_extension}'"
     end
   end
 end
